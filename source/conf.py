e592eab62cc815d22ca0da980ceac3f96589f91a
Todd Pataky <todd@Todds-MacBook.local>
2016-05-01 13:52:54 +0900

    Testing bootstrap theme and custom html layout
    Testing bootstrap theme and custom html layout

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-
+import sphinx_bootstrap_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -114,12 +114,22 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+
+html_theme_options = {'navbar_title': "    ",
+                      'bootstrap_version': "3",
+                      'navbar_site_name': "Site",
+                      'navbar_pagenav': True,
+                      'navbar_fixed_top': "true",
+                      'bootswatch_theme': "",
+                      'navbar_sidebarrel': False}
+
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
