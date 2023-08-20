2f5c2f5212b161647bb23bf928e9c2774dc0980e
Todd Pataky <0todd0@gmail.com>
2023-08-20 09:41:11 +0900

    {uneqvar} Prepare to merge T and F GLM classes

---
+++ src/spm1d/stats/f.py
@@ -0,0 +1,15 @@
+'''
+ANOVA tests (using F contrasts).
+'''
+
+# Copyright (C) 2023  Todd Pataky
+
+
+
+import numpy as np
+from . _dec import appendargs, checkargs
+
+
+
+
+
--- src/spm1d/stats/t.py
+++ src/spm1d/stats/t.py
@@ -1,54 +1,38 @@
 '''
-One- and two sample tests.
+One- and two sample tests (using T contrasts).
 '''
 
 # Copyright (C) 2023  Todd Pataky
 
 
 
-# import numpy as np
-# from .. _spmcls import SPM0D
-
-# from . factors import Factor
-# from . contrasts import Contrast
-
-# from .. _la import rank
-# from ... util import array2shortstr, dflist2str, df2str, DisplayParams
-# eps = np.finfo(float).eps   #smallest float, used to avoid divide-by-zero errors
-
-
-
 import numpy as np
-# from . _datachecks import checkargs
 from . _dec import appendargs, checkargs
-
-
 
 
 
 
 def _assemble_spm_objects(design, model, fit, teststat, roi=None):
 	if fit.dvdim==0:
-		# from .. _spmcls import SPM0D
-		from spm1d.stats._spmcls import SPM0D
-		# spm = [SPM0D(r, design, fit, c)  for r,c in zip(results, design.contrasts)]
+		from . _spmcls import SPM0D
 		spm = SPM0D(design, model, fit, teststat)
 	else:
-		# from .. _spmcls import SPM1D
-		from spm1d.stats._spmcls import SPM1D
+		from . _spmcls import SPM1D
 		spm = SPM1D(design, model, fit, teststat)
-		# spm = [SPM1D(design, model, fit, s, roi)  for s in teststats]
-	# if len(spm)==1:
-	# 	spm = spm[0]
-	# else:
-	# 	from .. _spmcls import SPMFList
-	# 	spm = SPMFList( spm )
 	return spm
 	
 
 
 
-def glm(y, X, c, QQ=None):
+def glm(y, X, c, ctype='T', QQ=None):
+	'''
+	General Linear Model
+	
+	Most general user-facing hypothesis testing function
+	
+	Note:  all built-in tests (e.g. ttest2, anova1rm, etc.)
+	represent specific cases of this glm function.
+	'''
 	from . core.models import GeneralLinearModel
 	model     = GeneralLinearModel()
 	model.set_design_matrix( X )
@@ -69,30 +53,16 @@ def regress(y, x, roi=None):
 	spm    = _assemble_spm_objects(design, model, fit, teststat)
 	spm.r  = spm.z / (  (spm.design.J - 2 + spm.z**2)**0.5)   # t = r * ((J-2)/(1-r*r) )**0.5
 	return spm
-	
-	# spm.r          = spm.z / (  (J - 2 + spm.z**2)**0.5)   # t = r * ((J-2)/(1-r*r) )**0.5
-	
-	
-	# # _datachecks.check('regress', Y, x)
-	# J              = y.shape[0]
-	# X              = np.ones((J,2))
-	# X[:,0]         = x
-	# c              = (1,0)
-	# spm            = glm(y, X, c)
-	# spm.r          = spm.z / (  (J - 2 + spm.z**2)**0.5)   # t = r * ((J-2)/(1-r*r) )**0.5
-	# return spm
 
 
 
 @appendargs
 @checkargs
 def ttest(y, mu=0, roi=None):
-	# _datachecks.check('ttest', y, mu)
 	from . core.designs import TTEST
 	design    = TTEST(y, mu)
 	model,fit,teststat = glm(y, design.X, design.contrasts[0].C)
 	return _assemble_spm_objects(design, model, fit, teststat)
-	# return model,fit,teststat
 	
 	
 
@@ -105,7 +75,6 @@ def ttest2(y0, y1, roi=None):
 	design    = TTEST2(n0, n1)
 	model,fit,teststat = glm(y, design.X, design.contrasts[0].C)
 	return _assemble_spm_objects(design, model, fit, teststat)
-	# return model,fit,teststat
 
 
 @checkargs
