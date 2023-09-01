2aeb56cc6a3b4209230fc64ffae6b0bb694767b7
Todd Pataky <0todd0@gmail.com>
2023-09-01 13:03:13 +0900

    {uneqvar} Extend _glm_design to other t.py tests

--- src/spm1d/stats/t.py
+++ src/spm1d/stats/t.py
@@ -8,7 +8,6 @@ One- and two sample tests (using T contrasts).
 
 import numpy as np
 from . _dec import appendargs, checkargs
-# from . _la import rank
 
 
 
@@ -26,6 +25,8 @@ def _assemble_spm_objects(design, model, fit, teststat, roi=None):
 def _glm_design(y, design, equal_var=False, roi=None):
 	'''
 	Design object-based interface to glm
+	
+	This is an internal function that is NOT meant for users.
 	'''
 	QQ         = design.get_variance_model( equal_var=equal_var )
 	spm        = glm(y, design.X, design.contrasts[0].C, ctype='T', QQ=QQ, roi=roi)
@@ -44,22 +45,13 @@ def glm(y, X, c, ctype='T', QQ=None, roi=None):
 	'''
 	from . glmc.designs import GLM
 	from . glmc.models import GeneralLinearModel
-	from . glmc._la import rank
+	# from . glmc._la import rank
 	# df0       = 1, X.shape[0] - rank(X)
 	design    = GLM(X, c)
 	model     = GeneralLinearModel(X, design.df0, QQ)
 	fit       = model.fit( y )
 	teststat  = fit.calculate_t_stat( c, roi=roi )
 	return _assemble_spm_objects(design, model, fit, teststat, roi=roi)
-	# if fit.dvdim==0:
-	# 	from . _spmcls import SPM0D
-	# 	spm = SPM0D(design, model, fit, teststat)
-	# else:
-	# 	from . _spmcls import SPM1D
-	# 	spm = SPM1D(design, model, fit, teststat)
-	# return spm
-	# return design, model, fit, teststat
-
 
 
 
@@ -67,19 +59,9 @@ def glm(y, X, c, ctype='T', QQ=None, roi=None):
 @checkargs
 def regress(y, x, roi=None):
 	from . glmc.designs import REGRESS
-	design = REGRESS(x)
-	model,fit,teststat = glm(y, design.X, design.contrasts[0].C)
-	spm    = _assemble_spm_objects(design, model, fit, teststat)
+	spm    = _glm_design(y, REGRESS(x) , roi)
 	spm.r  = spm.z / (  (spm.design.J - 2 + spm.z**2)**0.5)   # t = r * ((J-2)/(1-r*r) )**0.5
-	# df0    = design.get_df0(y.shape[0])
-	# print(df0)
 	return spm
-
-
-
-
-
-	
 
 
 @appendargs
@@ -87,58 +69,15 @@ def regress(y, x, roi=None):
 def ttest(y, mu=0, roi=None):
 	from . glmc.designs import TTEST
 	return _glm_design(y-mu, TTEST( y.shape[0] ) , roi)
-	# spm       = glm( y-mu, design.X, design.contrasts[0].C, ctype='T', )
-	# return _glm( y - mu , TTEST( y.shape[0] ) )
-	# # df0       = design.get_df0(y.shape[0])
-	# model,fit,teststat = glm(y-mu, design.X, design.contrasts[0].C)
-	#
-	# return _assemble_spm_objects(design, model, fit, teststat)
-	
-	
-
-# @appendargs
-# @checkargs
-# def ttest2(y0, y1, equal_var=False, roi=None):
-# 	from . designs import TTEST2
-# 	from .. _cov import CovarianceModel
-# 	n0,n1     = y0.shape[0], y1.shape[0]
-# 	y         = np.hstack( (y0,y1) ) if (y0.ndim==1) else np.vstack(  (y0, y1)  )
-# 	design    = TTEST2(n0, n1)
-# 	if equal_var:
-# 		QQ    = None
-# 		cmodel = CovarianceModel(design.X)
-# 		# cmodel.add_constant_var()
-# 	else:
-# 		cmodel = CovarianceModel(design.X)
-# 		cmodel.add_group_vars()
-# 		# model.add_autocorr()
-# 		QQ    = cmodel.get_model()
-#
-# 	model,fit,teststat = glm(y, design.X, design.contrasts[0].C, QQ=QQ)
-# 	return _assemble_spm_objects(design, model, fit, teststat)
 
 
 @appendargs
 @checkargs
 def ttest2(y0, y1, equal_var=False, roi=None):
 	from . glmc.designs import TTEST2
-	# from .. _cov import CovarianceModel
 	n0,n1     = y0.shape[0], y1.shape[0]
 	y         = np.hstack( (y0,y1) ) if (y0.ndim==1) else np.vstack(  (y0, y1)  )
-	design    = TTEST2(n0, n1)
-	QQ        = design.get_variance_model( equal_var=equal_var )
-	# if equal_var:
-	# 	QQ    = None
-	# 	cmodel = CovarianceModel(design.X)
-	# 	# cmodel.add_constant_var()
-	# else:
-	# 	cmodel = CovarianceModel(design.X)
-	# 	cmodel.add_group_vars()
-	# 	# model.add_autocorr()
-	# 	QQ    = cmodel.get_model()
-
-	model,fit,teststat = glm(y, design.X, design.contrasts[0].C, QQ=QQ)
-	return _assemble_spm_objects(design, model, fit, teststat)
+	return _glm_design(y, TTEST2(n0, n1) , equal_var, roi)
 
 
 @checkargs
@@ -147,3 +86,29 @@ def ttest_paired(y0, y1, roi=None):
 
 
 
+'''
+Below is (commented) development code to more clearly
+show processing flow for an example function (ttest2)
+'''
+
+# @appendargs
+# @checkargs
+# def ttest2(y0, y1, equal_var=False, roi=None):
+# 	from . glmc.designs import TTEST2
+# 	# from .. _cov import CovarianceModel
+# 	n0,n1     = y0.shape[0], y1.shape[0]
+# 	y         = np.hstack( (y0,y1) ) if (y0.ndim==1) else np.vstack(  (y0, y1)  )
+# 	design    = TTEST2(n0, n1)
+# 	QQ        = design.get_variance_model( equal_var=equal_var )
+# 	# if equal_var:
+# 	# 	QQ    = None
+# 	# 	cmodel = CovarianceModel(design.X)
+# 	# 	# cmodel.add_constant_var()
+# 	# else:
+# 	# 	cmodel = CovarianceModel(design.X)
+# 	# 	cmodel.add_group_vars()
+# 	# 	# model.add_autocorr()
+# 	# 	QQ    = cmodel.get_model()
+#
+# 	model,fit,teststat = glm(y, design.X, design.contrasts[0].C, QQ=QQ)
+# 	return _assemble_spm_objects(design, model, fit, teststat)
